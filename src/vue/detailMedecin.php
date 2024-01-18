8f90e0edd6aa36f411dc9454de6bfc9f112654d2
Titouan <titouan.brando@etu.iut-tlse3.fr>
2024-01-18 06:28:02 +0100

    J'ai fait des trucs mais jsp quoi

--- src/vue/detailMedecin.php
+++ src/vue/detailMedecin.php
@@ -15,7 +15,7 @@ require_once(__DIR__.'/head.php'); ?>
     </div>
     <a href="/index.php?action=modifMedecin&id=<?=$id?>" class="btn btn-primary">Modifier</button>
     <a href="/index.php?action=deleteMedecin&id=<?=$id?>" class="btn btn-danger">Supprimer</button>
-    <a href="/index.php?action=medecins" class="btn btn-seconday">Retour à la liste</a>
+    <a href="/index.php?action=medecins" class="btn btn-secondary">Retour à la liste</a>
   </div>
   <?php
     require('footer.html');
--- src/vue/detailRdv.php
+++ src/vue/detailRdv.php
@@ -30,7 +30,7 @@ require_once(__DIR__.'/head.php'); ?>
     </div>
     <a href="/index.php?action=modifRdv&idMedecin=<?=$rdv->getMedecin()->getPersonne()->getIdPersonne().'&idUsager='.$rdv->getUsager()->getPersonne()->getIdPersonne().'&dateHeure='.$rdv->getDateHeureDebut()->format('Y-m-d H:i')?>"class="btn btn-primary">Modifier</button>
     <a href="/index.php?action=deleteRdv&idMedecin=<?=$rdv->getMedecin()->getPersonne()->getIdPersonne().'&idUsager='.$rdv->getUsager()->getPersonne()->getIdPersonne().'&dateHeure='.$rdv->getDateHeureDebut()->format('Y-m-d H:i')?>"class="btn btn-danger">Supprimer</button>
-    <a href="/index.php?action=rdvs" class="btn btn-seconday">Retour à la liste</a>
+    <a href="/index.php?action=rdvs" class="btn btn-secondary">Retour à la liste</a>
   </div>
   <?php
     require('footer.html');
